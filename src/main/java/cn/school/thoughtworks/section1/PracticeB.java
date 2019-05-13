f8f560a260ff531a1ce95bdced21cc3fb3211997
huyf <spyssor@gmail.com>
2019-05-13 14:51:43 +0800

    Complete PracticeA and pass the PracticeATest

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,23 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> res = new ArrayList<>();
+
+        for (List<String> list : collection2){
+            for (int i = 0; i < collection1.size(); i ++){
+                for (int j = 0; j < list.size(); j ++){
+                    if (collection1.get(i).equals(list.get(j)) && !res.contains(collection1.get(i))){
+                        res.add(collection1.get(i));
+                    }
+                }
+            }
+        }
+
+        return res;
     }
 }
--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,23 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> res = new ArrayList<>();
+        List<String> list = collection2.get("value");
+
+        for (int i = 0; i < collection1.size(); i ++){
+            for (int j = 0; j < list.size(); j ++){
+                if (collection1.get(i).equals(list.get(j)) && !res.contains(collection1.get(i))){
+                    res.add(collection1.get(i));
+                }
+            }
+        }
+
+        return res;
     }
 }
