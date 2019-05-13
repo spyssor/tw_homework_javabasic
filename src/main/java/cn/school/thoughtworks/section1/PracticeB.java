e322f58c0d132fa2020ab5501afa859785631199
huyf <spyssor@gmail.com>
2019-05-13 14:58:12 +0800

    Complete section1 and pass the section1 test

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -17,7 +17,6 @@ public class PracticeB {
                 }
             }
         }
-
         return res;
     }
 }
--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -17,7 +17,6 @@ public class PracticeC {
                 }
             }
         }
-
         return res;
     }
 }
