9d999285e6f3a9190eaad7e49f348e66384f19de
huyf <spyssor@gmail.com>
2019-05-13 16:43:30 +0800

    Fix section2 PracticeB a small bug that cannot plus  Double digits in map

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -17,7 +17,7 @@ public class PracticeB {
                     res.put(element, 1);
                 }
             } else {
-                if (element.charAt(1) == '-' && element.length() == 3){
+                if (element.charAt(1) == '-'){
                     if (res.containsKey(element.charAt(0)+"")) {
                         res.put(element.charAt(0)+"", res.get(element.charAt(0)+"") + Integer.parseInt(element.substring(2)));
                     } else {
